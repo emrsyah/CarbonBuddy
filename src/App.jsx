1ebfefd7593e3d5e6207279f0197ca5f8c7deacb
Emirsyah <muhammademir48@gmail.com>
2022-07-23 15:07:32 +0700

    add app route guard, add loading ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Layout from "./pages/app/Layout";
 import Home from "./pages/Home";
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
+import Tracker from "./pages/app/Tracker";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/app" element={<Layout />}></Route>
+          <Route path="app" element={<Layout />}>
+            <Route index element={<Tracker />} />
+            <Route path="tracker" element={<Tracker />} />
+          </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </RecoilRoot>
   );
 }
 
--- src/pages/app/Layout.jsx
+++ src/pages/app/Layout.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { userAtom } from "../../atoms/userAtom";
+import { auth } from "../../firebase";
+import lottieJson from "../../assets/loading.json";
+import Lottie from "lottie-web";
 
 function Layout() {
-  return (
-    <div>Layout</div>
-  )
+  const setUser = useSetRecoilState(userAtom);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    try {
+      onAuthStateChanged(auth, async (user) => {
+        if (!user) {
+          navigate("/", { replace: true });
+          return;
+        }
+
+        setUser({
+          uid: user.uid,
+          displayName: user.displayName,
+          image: user.photoURL,
+          email: user.email,
+        });
+      });
+    } catch (err) {
+      console.error(err);
+      navigate("/");
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    // console.count("loaded")
+    const instance = Lottie.loadAnimation({
+      container: document.querySelector("#lottie-container"),
+      animationData: lottieJson,
+    });
+    return () => instance.destroy();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-[100vh] flex-col">
+        {/* <img src={loading} alt="" /> */}
+        {/* <img src={logo} alt="" className="h-14" /> */}
+        <h6 className="text-2xl font-semibold">CarbonBuddy🌏</h6>
+        <div id="lottie-container" className="w-28" />
+        {/* {""} */}
+        {/* <div>loading</div> */}
+      </div>
+    );
+  } else {
+    return (
+      <main>
+        <h1>Layout</h1>
+        <Outlet />
+      </main>
+    );
+  }
 }
 
-export default Layout
+export default Layout;
