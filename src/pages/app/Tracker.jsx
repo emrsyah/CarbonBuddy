9d40e7b0fa7b48e43029db97df28c2ebd07d79fc
Emirsyah <muhammademir48@gmail.com>
2022-07-24 08:18:09 +0700

    challenges randomizer from api data added

--- src/pages/app/Tracker.jsx
+++ src/pages/app/Tracker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import PageNavigation from "../../components/PageNavigation";
 import { Icon } from "@iconify/react";
@@ -7,7 +7,7 @@ import Challenges from "../../components/Challenges";
 import { useLocation } from "react-router-dom";
 
 const data = [1, 2, 3, 4, 5, 6, 7];
-const challenges = [
+const challengesMe = [
   "reduce water use for the next 7 days",
   "drink off reusable bottles for the whole day",
   //   "throw recyclable objects in recycle bins",
@@ -15,10 +15,22 @@ const challenges = [
 
 function Tracker() {
   const location = useLocation();
+  const [challenges, setChallenges] = useState("");
 
   useEffect(() => {
     if (!location.pathname.includes("/tracker")) navigate("/app/home");
+    getChallenges()
   }, []);
+
+  const getChallenges= async () => {
+    const id = Math.floor(Math.random() * 22) + 1;
+    const res = await fetch(
+      "https://x8ki-letl-twmt.n7.xano.io/api:e4LWH0US/challenges/" + id
+    );
+    const json = await res.json();
+    setChallenges({ ...json });
+    // console.log(json)
+  };
 
   return (
     <>
@@ -30,12 +42,16 @@ function Tracker() {
 
         {/* CHALLENGES GENERATOR */}
         <div className="pb-2 my-8 border-b-[1px] border-b-gray-700 flex items-center  justify-between">
-          <h5 className="text-xl font-medium">Our Challenges From API</h5>
+          <h5 className="text-xl font-medium">
+            {challenges === "" ? "Generating Some Challenges..." : <>{challenges.title}</>}
+          </h5>
           <div className="flex items-center gap-3">
             <button className="bg-blue-500 hover:bg-blue-600 py-2 px-5 font-medium rounded">
               Accept Challenge
             </button>
-            <button className="bg-white text-blue-600 p-[6px] hover:bg-gray-200 rounded">
+            <button 
+            onClick={()=>getChallenges()}
+            className="bg-white text-blue-600 p-[6px] hover:bg-gray-200 rounded">
               <Icon icon="ic:sharp-restart-alt" width={28} />
             </button>
           </div>
@@ -78,7 +94,7 @@ function Tracker() {
               ))}
             </div>
           </div>
-          {challenges.map((c) => (
+          {challengesMe.map((c) => (
             <Challenges c={c} data={data} />
           ))}
         </div>
